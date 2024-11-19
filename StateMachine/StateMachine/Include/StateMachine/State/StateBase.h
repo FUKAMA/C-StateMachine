517ef71709b968db77fb3148956acff8437e7d0f
super_fkmt <kd1362203@st.kobedenshi.ac.jp>
2024-11-19 12:25:47 +0900

    update:StateBaseクラスと基本の仮想関数を作成

--- StateMachine/StateMachine/Include/StateMachine/State/StateBase.h
+++ StateMachine/StateMachine/Include/StateMachine/State/StateBase.h
@@ -1,1 +1,19 @@
 ﻿#pragma once
+
+namespace stmc
+{
+	template<typename OwnerType>
+	class StateBase
+	{
+	public:
+
+		virtual void Begin(OwnerType* a_pOwner) {}
+
+		virtual void Update(OwnerType* a_pOwner) {}
+
+		virtual void End(OwnerType* a_pOwner) {}
+
+	private:
+
+	};
+}
